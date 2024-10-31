18129d3b552c75d16ac6f81a9d289f4467b38f45
Alik <moisesalik01@gmail.com>
2024-10-30 22:17:15 -0500

    pantalla de inicio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,31 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				titulos: ['Staatliches', 'serif'],
+				parrafos: ['Roboto Condensed', 'sans-serif'],
+			}
+		},
 	},
 	plugins: [
 		require('daisyui'),
 	],
 	daisyui: {
-		themes: ["light"],
+		themes: [
+			{
+				mytheme: {
+					"primary": "#4ade80",
+					"secondary": "#67e8f9",
+					"accent": "#BE3DF9",
+					"neutral": "#001b23",
+					"base-100": "#f3f4f6",
+					"info": "#4b5563",
+					"success": "#4ade80",
+					"warning": "#fef08a",			  
+					"error": "#f87171",
+				},
+			}
+		],
 	},
 }
